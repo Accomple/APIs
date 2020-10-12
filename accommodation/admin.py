db7ec32e030294da07f84f4bdc14f10777fd3460
HeptaDecane <sooraj1999vs@gmail.com>
2020-10-12 20:28:42 +0530

    added booking, property_deed models

--- accommodation/admin.py
+++ accommodation/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Building)
+admin.site.register(Room)
+admin.site.register(Booking)
+admin.site.register(RoomPhoto)
+admin.site.register(RoomPerk)
+admin.site.register(BuildingPerk)
+admin.site.register(PropertyDeed)
--- accommodation/models.py
+++ accommodation/models.py
@@ -1,54 +1,81 @@
 from django.db import models
-
-# Create your models here.
-# from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import AbstractUser
-from authentication.models import Owner
+from authentication.models import Owner, Seeker
 
 
 class Building(models.Model):
-    building_name = models.CharField(max_length=20)
-    coordinate_x = models.DoubleField(null=False)
-    coordinate_y = models.DoubleField(null=False)
-    landmark = models.CharField(max_length=40)
-    in_time = models.DateTimeField(null=True, blank=True)
-    address = models.CharField(max_length=40)
+    building_name = models.CharField(max_length=32)
+    street = models.CharField(max_length=32)
+    city = models.CharField(max_length=16)
+    state = models.CharField(max_length=16)
+    zip_code = models.CharField(max_length=6)
+    landmark = models.CharField(max_length=32)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    in_time = models.TimeField(null=True, blank=True)
 
     def __str__(self):
-        return self.id
+        return self.building_name
 
-class Building_Perks(models.Model):
-    id=models.ForeignKey(Building, on_delete=models.CASCADE, null=True)
-    description = models.CharField(max_length=40)
-    building = models.ManyToManyField(Building, on_delete=models.CASCADE, null=True)
+
+class BuildingPerk(models.Model):
+    building = models.ManyToManyField(Building)
+    description = models.CharField(max_length=32)
 
     def __str__(self):
-        return self.building.id
+        return self.description
+
 
 class Room(models.Model):
-    building = models.ForeignKey(Building, on_delete=models.CASCADE, null=True)
-    title= models.CharField(max_length=40)
-    rent = models.DoubleField(null=False)
-    room_no=models.IntegerField(null=False)
-    main_pic=models.ImageField(null=True, blank=True, upload_to='main_photo')
-    description =models.CharField(max_length=40)
-    owner=models.ForeignKey(Owner, on_delete=models.CASCADE, null=True)
+    GENDER_LABELS = [('M', 'Male'), ('F', 'Female'), ('U', 'Unisex')]
+
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    title = models.CharField(max_length=32)
+    rent = models.FloatField(null=False)
+    room_no = models.IntegerField(null=False)
+    gender_label = models.CharField(max_length=1, choices=GENDER_LABELS)
+    occupancy = models.IntegerField()
+    display_pic = models.ImageField(null=True, blank=True, upload_to='display_pics')
     is_booked = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.id
+        return self.title
 
-class Room_Perks(models.Model):
-    description = models.CharField(max_length=40)
-    room = models.ManyToManyField(Room, on_delete=models.CASCADE, null=True)
+
+class RoomPerk(models.Model):
+    room = models.ManyToManyField(Room)
+    description = models.CharField(max_length=32)
 
     def __str__(self):
-        return self.room.id
+        return self.description
 
-class Room_Photos(models.Model):
-    photo = models.ImageField(null=True, blank=True, upload_to='room_photo')
-    room= models.ForeignKey(Room, on_delete=models.CASCADE, null=True)
 
+class RoomPhoto(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='room_photos')
+
+    def __str__(self):
+        return self.photo.path
+
+
+class Booking(models.Model):
+    booking_no = models.CharField(max_length=32, unique=True)
+    expiration = models.DateTimeField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(Seeker, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.booking_no
+
+
+class PropertyDeed(models.Model):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    registration_no = models.CharField(max_length=32)
+    issue_date = models.DateField()
+    expiry_date = models.DateField(null=True, blank=True)
+    document = models.FileField(upload_to='property_deeds')
+
+    def __str__(self):
+        return self.registration_no
