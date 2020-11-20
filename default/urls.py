424e9f9c39b30671bfe6c6d91a8f5bd579dc1b60
Sooraj V S <sooraj1999vs@gmail.com>
2020-11-21 00:42:53 +0530

    unclosed urls

--- default/urls.py
+++ default/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 app_name = "default"
 urlpatterns = [
-    path('active_cities/names', ActiveCityNames.as_view(), name='active_city_names'),
-    path('active_cities/zip_codes', ActiveCityZipCodes.as_view(), name='active_city_zip_codes'),
+    path('active_cities/names/', ActiveCityNames.as_view(), name='active_city_names'),
+    path('active_cities/zip_codes/', ActiveCityZipCodes.as_view(), name='active_city_zip_codes'),
 ]
